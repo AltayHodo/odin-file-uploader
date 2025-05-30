ecfaad50b34901c98844d9ab9f6c44465f203002
Altay Hodoglugil <altay.hodo@gmail.com>
2025-05-30 16:48:37 -0700

    Initial commit

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+const assetsPath = path.join(__dirname, 'public');
+app.use(express.static(assetsPath));
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(express.urlencoded({ extended: true }));
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}!`);
+});
